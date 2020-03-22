29fb22a1e0fbc82a6f651e79480b9d1840b8edc4
Conor <conor.heeney56@gmail.com>
2020-03-22 21:49:35 +0000

    Added remove functionality and length and min methods

--- pq.py
+++ pq.py
@@ -45,31 +45,62 @@ class Heap:
             parent = (index-1) // 2
             if self.structure[index].key < self.structure[parent].key:
                 self.structure[index], self.structure[parent] = self.structure[parent], self.structure[index]
-            self.bubbleUp(parent)
+                self.bubbleUp(parent)
+
+    def remove_min(self):
+        min = self.structure[self.root]
+        self.structure[self.root], self.structure[self.last] = self.structure[self.last], self.structure[self.root]
+        self.structure.pop(self.last)
+        self.last -= 1
+        self.size -= 1
+        self.bubbleDown(self.root)
+        return min
+
+    def bubbleDown(self, index):
+        if 2 * index + 2 <= self.size:
+            lc = 2 * index + 1
+            rc = 2 * index + 2
+            if self.structure[lc].key < self.structure[rc].key:
+                swap = 2 * index + 1
+            else:
+                swap = 2 * index + 2
+        elif 2 * index + 1 <= self.size:
+            swap = 2 * index + 1
+        else:
+            return None
+        if self.structure[index].key > self.structure[swap].key:
+            self.structure[index], self.structure[swap] = self.structure[swap], self.structure[index]
+            self.bubbleDown(swap)
+
+
 
 heap = Heap()
 e1 = heap.add(25, 1)
-print(heap)
+print(heap, heap.last)
 e2 = heap.add(27, 1)
-print(heap)
+print(heap, heap.last)
 e3 = heap.add(29, 1)
-print(heap)
+print(heap, heap.last)
 e4 = heap.add(28, 1)
-print(heap)
+print(heap, heap.last)
 e5 = heap.add(35, 1)
-print(heap)
+print(heap, heap.last)
 e6 = heap.add(49, 1)
-print(heap)
+print(heap, heap.last)
 e7 = heap.add(43, 1)
-print(heap)
+print(heap, heap.last)
 e8 = heap.add(37, 1)
-print(heap)
+print(heap, heap.last)
 e9 = heap.add(22, 1)
-print(heap)
+print(heap, heap.last)
 e10 = heap.add(50, 1)
-print(heap)
+print(heap, heap.last)
 e11 = heap.add(12, 1)
-print(heap)
+print(heap, heap.last)
 e12 = heap.add(33, 1)
-print(heap)
+print(heap, heap.last)
+heap.remove_min()
+print(heap, heap.last)
+heap.remove_min()
+print(heap, heap.last)
 
