61abeaf70b91f5c21e1b726b31f54275fc933dac
Conor <conor.heeney56@gmail.com>
2020-04-03 00:36:00 +0100

    Finished implementing dijkstra

---
+++ __init__.py
@@ -0,0 +1,42 @@
+from graph import *
+from dijkstra import *
+
+def graphreader(filename):
+    """ Read and return the route map in filename. """
+    graph = Graph()
+    file = open(filename, 'r')
+    entry = file.readline() #either 'Node' or 'Edge'
+    num = 0
+    while entry == 'Node\n':
+        num += 1
+        nodeid = int(file.readline().split()[1])
+        vertex = graph.addVertex(nodeid)
+        entry = file.readline() #either 'Node' or 'Edge'
+    print('Read', num, 'vertices and added into the graph')
+    num = 0
+    while entry == 'Edge\n':
+        num += 1
+        source = int(file.readline().split()[1])
+        sv = graph.getVertexByLabel(source)
+        target = int(file.readline().split()[1])
+        tv = graph.getVertexByLabel(target)
+        length = float(file.readline().split()[1])
+        edge = graph.addEdge(sv, tv, length)
+        file.readline() #read the one-way data
+        entry = file.readline() #either 'Node' or 'Edge'
+    print('Read', num, 'edges and added into the graph')
+    return graph
+
+def println(dic):
+    print("----------")
+    for vertex in dic:
+        print("%s: (%i, %s)" % (vertex, closed[vertex][0], closed[vertex][1]))
+    print("----------")
+
+graph = graphreader("C:/Users/conor/Desktop/vscode/algorithms2/graph/simplegraph1.txt")
+closed = dijkstra(graph, 1)
+println(closed)
+
+graph = graphreader("C:/Users/conor/Desktop/vscode/algorithms2/graph/simplegraph2.txt")
+closed = dijkstra(graph, 14)
+println(closed)
---
+++ dijkstra.py
@@ -0,0 +1,34 @@
+from apq import  *
+from graph import *
+
+def dijkstra(graph, s):
+    open = APQ()
+    locs = {}
+    closed = {}
+    preds = {s: None}
+
+    svertex = graph.getVertexByLabel(s)
+    elt = open.add(0, s)
+    locs[s] = elt
+    while open.length() != 0:
+        vElt = open.remove_min()
+        v = vElt.value
+        vvertex = graph.getVertexByLabel(v)
+        vcost = vElt.key
+        locs.pop(v)
+        predecessor = preds.pop(v)
+        closed[v] = (vcost, predecessor)
+        edges = graph.getEdges(graph.getVertexByLabel(v))
+        for edge in edges:
+            w = edge.opposite(vvertex)
+            if w.element() not in closed:
+                ecost = edge.element()
+                newcost = vcost + ecost
+                if w.element() not in locs:
+                    preds[w.element()] = v
+                    elt = open.add(newcost, w.element())
+                    locs[w.element()] = elt
+                elif newcost < locs[w.element()].key:
+                    preds[w.element()] = v
+                    locs[w.element()].key = newcost
+    return closed
